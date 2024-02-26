06cab15b82ef854d69f70afa1e5b267b1c86591b
zawarudobngdev <murilo.m@hotmail.com>
2024-02-26 19:41:34 -0300

    Endpoints CustomerController

---
+++ src/main/kotlin/bngdev/credit/application/system/dto/CustomerUpdateDto.kt
@@ -0,0 +1,21 @@
+package bngdev.credit.application.system.dto
+
+import bngdev.credit.application.system.entity.Customer
+import java.math.BigDecimal
+
+data class CustomerUpdateDto(
+    val firstName: String,
+    val lastName: String,
+    val income: BigDecimal,
+    val zipCode: String,
+    val street: String
+) {
+    fun toEntity(customer: Customer) : Customer {
+        customer.firstName = this.firstName
+        customer.lastName = this.lastName
+        customer.income = this.income
+        customer.address.zipCode = this.zipCode
+        customer.address.street = this.street
+        return customer
+    }
+}
